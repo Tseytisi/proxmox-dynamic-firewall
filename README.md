f88390c853154dafa6e445b5eac8adfcb0eef91d
Tseytisi <tseytisi@proton.me>
2026-05-03 15:16:01 +0200

    IPv4 implemented and tested

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# proxmox-dynamic-firewall
-Dynamically update Proxmox firewall IP entries using DNS
+# Unofficial Proxmox dynamic firewall service
+Dynamically update Proxmox firewall IP entries using DNS.
+
+Small script that periodically queries a given domain name, and resolves its IP-address. If the IP-address changes, the dynamic firewall entries for that domain are automatically updated. Handy for IP whitelists that contain non-static public IP-addresses. Best combined with DDNS.
+
+- Dynamic entries are visible and updateable from the Proxmox web GUI
+- Only entries explicitly marked dynamic are updated, even if other entries contain the same IP-address
+- Works with any 'A' and 'AAAA' type DNS record, even for private-range IP-addresses
+- Only writes updates if an IP-address actually changed. The firewall is not reloaded more than necessary.
+- Disabled firewall entries are not updated
+
+_I am not affiliated with the Proxmox team. This is not official software and comes with no warranty. Proxmox updates may break this software._
+
+_IPv6 is not fully implemented yet_
+
+# How to use
+After [installation](#how-to-install), in either the web interface or the `.fw` files on a node (located at `/etc/pve/firewall/`):
+
+Add the following to the comment of an entry: `[DYN-IP <type> <domain>]`, where:
+- `type` is either `A` (IPv4) or `AAAA` (IPv6)
+- `domain` is any DNS-resolvable domain or subdomain
+
+Other comments are still allowed.
+
+Example (using private IP-ranges as example):
+```
+[IPSET remote_engineers]
+
+10.20.30.40 # John's house [DYN-IP A example.com]
+10.0.0.50 # Static entry [regular comments in square-brackets are no problem]
+2001:db8::ff00:42:8329 # Alice's proxy [DYN-IP AAAA proxy.alice.example]
+
+[RULES]
+
+IN ACCEPT -p tcp -dport 8080 -source 10.10.60.10 -log nolog # [DYN-IP A home.dave.example]
+```
+
+There is no need to have the DNS record to point to an active service. An entry like `[DYN-IP A temp-location1.thecompany.com]` works completely fine even if the IP-address would be of no use in a web-browser or other application.
+
+# How to install
+The "service" consists of a single Bash script that needs no other resources to run. A systemd service and timer are included in the repository, but the only requirement is that the `pvedynfw.sh` script is executed periodically. Cron or any task scheduler would suffice too.
+
+Basic installation places `pvedynfw.sh` in `/usr/local/bin/`, and both `pvedynfw.service` and `pvedynfw.timer` in `/etc/systemd/system/`. After updating files for systemd, make sure to run `systemctl daemon-reload`. Then start the timer (and enable it for after reboots) with `systemctl enable --now pvedynfw.timer`.
+
+The script must be run as `root`, as the firewall files are not writeable by another user.
+- It is possible to run the script as a less-privileged user if some other script or user updates the firewall files manually. In this case, `$NO_OVERWRITE` should be set to `true`, which will write updated files to `/tmp/`.
+    - The user `www-data` is the only non-root user with read access (and does not have write access). Any other user would also need some other way to read the current firewall files.
+
+The script depends on a few commands to work (all included in a basic PVE installation as far as I'm aware):
+- Bash that supports POSIX Extended Regular Expressions
+- `dig` - For DNS lookups
+- `awk`
+- `grep` (GNU), must support `-P` Perl regular expressions
+
+The script logs any errors to STDERR. When running through systemd, these logs are visible through `journalctl -u pvedynfw.service`.
+
+# Behaviour
+- What if a query fails?
+    - If the DNS query does not return a valid IPv4/IPv6 address, either because the DNS server is unreachable or because the domain could not be resolved, those entries in the firewall are left as-is. Other dynamic IP entries will still be checked.
+- What if the query returns more than one address?
+    - By default, an error is logged and the firewall remains unchanged. It is possible to instead use the first address returned by the query, but this is disabled by default. It is (currently) not possible to use all returned IP-addresses.
+- What if the firewall rule contains more than one IP? Which one will be updated?
+    - The first IP-address of the target type in the rule will be updated. E.g:
+        - If the rule of a `[DYN-IP A ...]` contains more than one IPv4 address, the first IPv4 address will be updated
+        - If the rule of a `[DYN-IP A ...]` contains first an IPv6 address and then an IPv4 address, the IPv4 address will be updated
+        - If the rule of a `[DYN-IP AAAA ...]` contains more than one IPv6 address, the first IPv6 address will be updated
+        - If the rule of a `[DYN-IP AAAA ...]` contains first an IPv4 address and then an IPv6 address, the IPv6 address will be updated
+
+# Disclaimer
+_This software comes with absolutely no warranty._ A dynamic firewall is more error-prone than a static one, and this system is vulnerable to DNS attacks and DNS cache poisoning. Though the latter concern can be reduced by using DNSSEC. The attacker would also need to know the exact domain the script queries. And then still, a firewall is (almost) never the only line of defense. Security concerns are small, but be aware of them.
+
+This script was tested on Proxmox Virtual Environment 9.1, for a single node. It was not tested on a multi-node PVE datacentre. It should work on Proxmox Backup Server too, but this was also not tested.
+
+IPv6 was also not thoroughly tested, though should work very similarly to IPv4.
+
---
+++ pvedynfw.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+# Unofficial dynamic firewall rule updater based on IPs retrieved from DNS
+# Author: Tseytisi <tseytisi@proton.me>
+
+# Dependencies
+# - dig
+# - awk
+# - grep
+
+# This code is completely clanker-free
+
+# --- Settings ---
+# Enter every firewall file that should be checked for dynamic IP rules
+FIREWALL_FILES=(
+    # Unquoted, this line expands to every file in the firewall directory
+    /etc/pve/firewall/*
+    # For more fine-grained control, use manual entries like: (no commas!)
+    #"/etc/pve/firewall/cluster.fw"
+    #"/etc/pve/firewall/100.fw"
+    #"/etc/pve/firewall/101.fw"
+)
+
+# Log each check instead of just errors (prints to STDERR)
+VERBOSE=false
+
+# Sets behaviour for if the DNS response contains multiple IP address
+# false: ignore the response and do not update the firewall
+# true: take the first IP from the response
+ALLOW_MULTIPLE_RESPONSES=false
+
+# For testing only; writes updates to a temporary file and does not overwrite the actual firewall rules
+# Of course, firewall changes will not take effect, and every check will write a new updated file 
+NO_OVERWRITE=false
+# --- End: Settings ---
+
+# "Don't forget to euo pipefail because Bash will take every opportunity to delete your computer" ~ A wise man
+set -euo pipefail
+
+# A bunch of Regex (POSIX format :( )
+IPV4_matcher="[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}"
+IPV6_matcher="([[:blank:]]+(([[:xdigit:]]{1,4}:)+|:)(:|(:[[:xdigit:]]{1,4})+)[[:blank:]]+|[[:xdigit:]]{1,4}(:[[:xdigit:]]{1,4}){7})"
+
+# --- Functions --- 
+# Parameters:
+# 1: domain
+# 2: old IPv4
+check_ipv4 () {
+    response="$(dig "$1" A +noall +answer | awk '{if ($4 == "A") print $5}')"
+    if ! $ALLOW_MULTIPLE_RESPONSES && [ `printf "$response\n" | wc -l` -gt 1 ]; then
+        >&2 printf "Error: DNS lookup for '%s' returned more than one IPv4 address\n" "$1" 
+        return
+    fi
+    if [[ $response =~ $IPV4_matcher ]]; then
+        if $VERBOSE; then
+            >&2 printf "Received IPv4 response: %s\n" "${BASH_REMATCH}"
+        fi
+        if [[ $response != $2 ]]; then
+            echo "${BASH_REMATCH}"
+            return
+        elif $VERBOSE; then
+            >&2 echo Address did not change
+        fi
+    else
+        >&2 printf "Error: No (valid) response for DNS lookup of domain '%s'\n" "$1"
+        return
+    fi
+}
+# --- End: Functions --- 
+
+TEMP_FILE="/tmp/pvedynfw.tmp"
+
+# --- Script start ---
+# For each file
+for filepath in ${FIREWALL_FILES[@]}; do
+    if $VERBOSE; then
+        >&2 echo "Checking file $filepath"
+    fi
+    if [ ! -f "$filepath" ]; then
+        >&2 printf "Error: File not found '%s'\n" "$filepath"
+        continue
+    fi
+
+    first_change=true
+    source_file="$filepath"
+    # For every line in the file with [DYN-IP (A|AAAA) <domain>]
+    while read -r line; do
+        if [[ $line == "" ]]; then
+            if $VERBOSE; then
+                >&2 echo "Found no dynamic IP entries in file"
+            fi
+            continue
+        fi
+        if $VERBOSE; then
+            >&2 echo -e "\nChecking entry '$line'"
+        fi
+
+        # Do not update disabled firewall rules
+        if [[ $line == \|* ]]; then
+            if $VERBOSE; then
+                >&2 echo Skipping disabled entry
+            fi
+            continue
+        fi
+
+        # For whatever reason this regex cannot be in a variable
+        if [[ $line =~ \[DYN-IP[[:blank:]]+([[:alnum:]]+)[[:blank:]]+([^\][:blank:]]+)[[:blank:]]*\] ]]; then
+            rule="$(printf "$line" | cut -d '#' -f 1)"
+            record="${BASH_REMATCH[1]}"
+            domain="${BASH_REMATCH[2]}"
+            declare address replacement
+            case "$record" in
+                A)
+                    if [[ $rule =~ $IPV4_matcher ]]; then
+                        address="${BASH_REMATCH}"
+                        replacement="$(check_ipv4 "$domain" "$address")"
+                        if [ -z "$replacement" ]; then
+                            # Empty string return value is either no update
+                            # or some error; either way don't update the firewall
+                            continue
+                        fi
+
+                        # Sanity-check; it's very easy to return something extra from
+                        # a Bash function and this is the last check before we update
+                        # the firewall
+                        if [[ ! $replacement =~ $IPV4_matcher ]]; then
+                            >&2 printf "ERROR: Invalid return from IPv4 check '%s' (this is a bug, please report this)\n" "$replacement" 
+                            continue
+                        fi
+                    else
+                        >&2 printf "Error: Did not find a valid IPv4 address in rule on line: (file: %s) %s\n" "$filepath" "$line"
+                    fi
+                    ;;
+                AAAA)
+                    >&2 printf "IPv6 is not fully implemented yet :(\n"
+                    continue
+                    # TODO check if the Regex supports lines in IPsets that start with an IPv6 addr
+                    if [[ $rule =~ $IPV6_matcher ]]; then
+                        # Strip whitespace
+                        address="$(printf "${BASH_REMATCH}" | xargs)"
+                        echo "Found IPv6: $address"
+                    else
+                        >&2 printf "Error: Did not find a valid IPv6 address in rule on line: (file: %s) %s\n" "$filepath" "$line"
+                    fi
+                    ;;
+                *)
+                    >&2 printf "Error: Unrecognised DNS record type '%s'\n" "$record"
+                    continue
+            esac
+
+            if [ ! -z "$replacement" ]; then
+                # If we're here; we have a valid and new IP to update the firewall with
+                >&2 printf "Found new IP for domain '%s' (%s -> %s)\n" "$domain" "$address" "$replacement"
+
+                # Write an updated file and update the line we're checking
+                awk '$0 == orig { sub("'"$address"'", "'"$replacement"'") } { print }' orig="$line" "$source_file" > "$TEMP_FILE"
+
+                # First change, we read from the original file;
+                # any subsequent change, we read from the temporary file
+                # if we did this at least once, at the end, overwrite the original with the temp file
+                if $first_change; then
+                    first_change=false
+                    source_file="$TEMP_FILE"
+                fi
+            fi
+        else
+            >&2 printf "Error: Malformed dynamic IP entry (file %s): %s\n" "$filepath" "$line"
+        fi
+    done <<< "$(grep -P '\[DYN-IP[^\]]*\]' "$filepath")"
+
+    if ! $first_change; then
+        if [ -f "$TEMP_FILE" ]; then
+            # Only one write, and mv (on the same filesystem) is atomic
+            # So PVE should only notice one firewall update
+            if ! $NO_OVERWRITE; then
+                # Give the replacement file the same permissions as the current version
+                cp -p --attributes-only "$filepath" "$TEMP_FILE"
+                mv "$TEMP_FILE" "$filepath"
+                >&2 printf "Updated firewall file '%s'\n" "$filepath"
+            else
+                not_so_temp_file="/tmp/$(basename "$filepath").new"
+                mv "$TEMP_FILE" "$not_so_temp_file"
+                >&2 printf "Update for firewall '%s' written to '%s'\n" "$filepath" "$not_so_temp_file"
+            fi
+        else
+            >&2 printf "Firewall should have been updated but the changed file is gone (this is a bug, please report this)\n"
+        fi
+    fi
+done
